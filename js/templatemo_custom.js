7e3703a63086847563ccb78b19282435ff68dcf7
teamwhitevision <84313121+teamwhitevision@users.noreply.github.com>
2022-01-25 00:06:51 +0530

    Add files via upload

---
+++ js/templatemo_custom.js
@@ -0,0 +1,53 @@
+"use strict";
+
+jQuery(document).ready(function($){
+
+
+
+
+	/************** Gallery Hover Effect *********************/
+	$(".overlay").hide();
+
+	$('.gallery-item').hover(
+	  function() {
+	    $(this).find('.overlay').addClass('animated fadeIn').show();
+	  },
+	  function() {
+	    $(this).find('.overlay').removeClass('animated fadeIn').hide();
+	  }
+	);
+
+
+	/************** LightBox *********************/
+	$(function(){
+		$('[data-rel="lightbox"]').lightbox();
+	});
+
+
+	$("a.menu-toggle-btn").click(function() {
+	  $(".responsive_menu").stop(true,true).slideToggle();
+	  return false;
+	});
+ 
+    $(".responsive_menu a").click(function(){
+		$('.responsive_menu').hide();
+	});
+
+});
+
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+      'callback=initialize';
+  document.body.appendChild(script);
+}
+
+function initialize() {
+    var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(16.8251789,96.1439764)
+    };
+    var map = new google.maps.Map(document.getElementById('templatemo_map'),  mapOptions);
+}
